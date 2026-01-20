019aabeffefeb30371d89f4a65364bb04eac1f6b
millidavids <david.a.yurek@gmail.com>
2026-01-20 09:13:26 -0500

    Add --release flag to build_wasm.sh script
    Usage:
      ./build_wasm.sh          - Build in debug mode (faster compilation)
      ./build_wasm.sh --release - Build in release mode (optimized, slower compilation)

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 set -e
 
-echo "Building for WASM..."
-cargo build --target wasm32-unknown-unknown
+# Check for --release flag
+RELEASE_FLAG=""
+BUILD_TYPE="debug"
+if [ "$1" = "--release" ]; then
+    RELEASE_FLAG="--release"
+    BUILD_TYPE="release"
+    echo "Building for WASM (RELEASE MODE)..."
+else
+    echo "Building for WASM (debug mode)..."
+fi
+
+cargo build --target wasm32-unknown-unknown $RELEASE_FLAG
 
 echo "Running wasm-bindgen..."
 wasm-bindgen \
   --out-dir ./web \
   --out-name the_game \
   --target web \
-  ./target/wasm32-unknown-unknown/debug/the_game.wasm
+  ./target/wasm32-unknown-unknown/$BUILD_TYPE/the_game.wasm
 
 echo "WASM build complete! Files are in ./web/"
 echo "Open web/index.html in your browser to run the game."
