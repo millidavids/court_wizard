a4247d6ffde45cd9d61016f45c0002aa37f8dff8
millidavids <david.a.yurek@gmail.com>
2026-01-27 14:40:42 -0500

    Add README explaining game mechanics and objectives

--- docs/the_game_bg.wasm.d.ts
+++ docs/the_game_bg.wasm.d.ts
@@ -2,11 +2,11 @@
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
 export const main: (a: number, b: number) => number;
-export const wasm_bindgen__convert__closures_____invoke__h6c1614a914bf9051: (a: number, b: number, c: number) => void;
-export const wasm_bindgen__closure__destroy__h2a425feaef2e9212: (a: number, b: number) => void;
 export const wasm_bindgen__convert__closures_____invoke__h0a03e31b04822b99: (a: number, b: number, c: any) => void;
-export const wasm_bindgen__convert__closures_____invoke__h28b523b4b334baa7: (a: number, b: number) => void;
+export const wasm_bindgen__closure__destroy__h2a425feaef2e9212: (a: number, b: number) => void;
 export const wasm_bindgen__convert__closures_____invoke__h1548f2d8986e3878: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h6c1614a914bf9051: (a: number, b: number, c: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h28b523b4b334baa7: (a: number, b: number) => void;
 export const __wbindgen_malloc: (a: number, b: number) => number;
 export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
 export const __externref_table_alloc: () => number;
